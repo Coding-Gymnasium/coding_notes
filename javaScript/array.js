8c7f37249074897204673f06d0ecba4b7f4890d5
Nico Rithner <nicorithner@gmail.com>
2021-02-13 17:04:49 -0700

    Add javaScript notes. Array methods

---
+++ javaScript/array.js
@@ -0,0 +1,18 @@
+// Methods
+const myArray = [1,2,3,4];
+
+// map();
+myArray.map(el => el + 1)  // [2, 3, 4, 5]
+myArray.map(() => "b")  // ["b", "b", "b", "b"]
+myArray // [1, 2, 3, 4]
+
+// filter();
+
+
+// reduce();
+
+
+// find();
+
+
+// includes();,
